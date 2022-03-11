4333b2a060566c68887c61d8418c6029abc3713d
Mhmd <biboo.biboo97@gmail.com>
2022-03-11 07:00:55 +0200

    FIX: file paths

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,12 @@
 function navToggler(e) {
   const links = e.target.nextElementSibling;
   const handleLinks = (handleAria, handleImgSrc) => {
+    let path =
+      document.title.substr(16) === "Home"
+        ? "assets/images/nav/"
+        : "../assets/images/nav/";
     e.target.ariaLabel = `${handleAria} menu`;
-    e.target.firstElementChild.src = `assets/images/nav/icon-${handleImgSrc}.svg`;
+    e.target.firstElementChild.src = `${path}icon-${handleImgSrc}.svg`;
   };
 
   links.classList.toggle("show");
